b20e449eeeefc0a8ca320862692fe93c1c8ad2c3
Mark Chapman <mark.chapman@gmail.com>
2013-11-15 14:06:51 +0000

    Allow local override of substitution (by define with value 'true' or 'false')

--- tasks/grunt-expand-include.js
+++ tasks/grunt-expand-include.js
@@ -1,31 +1,32 @@
 /*
-**  grunt-expand-include -- Grunt Task for Expanding Include Directives
-**  Copyright (c) 2013 Ralf S. Engelschall <rse@engelschall.com>
-**
-**  Permission is hereby granted, free of charge, to any person obtaining
-**  a copy of this software and associated documentation files (the
-**  "Software"), to deal in the Software without restriction, including
-**  without limitation the rights to use, copy, modify, merge, publish,
-**  distribute, sublicense, and/or sell copies of the Software, and to
-**  permit persons to whom the Software is furnished to do so, subject to
-**  the following conditions:
-**
-**  The above copyright notice and this permission notice shall be included
-**  in all copies or substantial portions of the Software.
-**
-**  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-**  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-**  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-**  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-**  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-**  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-**  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+ **  grunt-expand-include -- Grunt Task for Expanding Include Directives
+ **  Copyright (c) 2013 Ralf S. Engelschall <rse@engelschall.com>
+ **
+ **  Permission is hereby granted, free of charge, to any person obtaining
+ **  a copy of this software and associated documentation files (the
+ **  "Software"), to deal in the Software without restriction, including
+ **  without limitation the rights to use, copy, modify, merge, publish,
+ **  distribute, sublicense, and/or sell copies of the Software, and to
+ **  permit persons to whom the Software is furnished to do so, subject to
+ **  the following conditions:
+ **
+ **  The above copyright notice and this permission notice shall be included
+ **  in all copies or substantial portions of the Software.
+ **
+ **  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ **  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ **  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ **  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ **  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ **  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ **  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 /* global module:  false */
 /* global require: false */
 
 var path  = require("path");
+var extend = require('node.extend');
 var chalk = require("chalk");
 
 module.exports = function (grunt) {
@@ -101,85 +102,91 @@ module.exports = function (grunt) {
                 txt = txt.replace(options.directiveSyntax.expand,
                     function (directive, name) {
 
-                    /*  skip special case  */
-                    if (name === "include")
-                        return directive;
+                        /*  skip special case  */
+                        if (name === "include")
+                            return directive;
 
-                    /*  resolve variable  */
-                    var txt = defines[name];
-                    if (typeof txt === "undefined") {
-                        if (options.onUndefinedVariable === "keep")
-                            txt = directive;
-                        else if (options.onUndefinedVariable === "empty")
-                            txt = "";
-                        else /* if (options.onUndefinedVariable === "error") */
-                            throw "Variable \"" + name + "\" not defined.";
-                    }
-                    return txt;
-                });
+                        /*  resolve variable  */
+                        var txt = defines[name];
+                        if (typeof txt === "undefined") {
+                            if (options.onUndefinedVariable === "keep")
+                                txt = directive;
+                            else if (options.onUndefinedVariable === "empty")
+                                txt = "";
+                            else /* if (options.onUndefinedVariable === "error") */
+                                throw "Variable \"" + name + "\" not defined.";
+                        }
+                        return txt;
+                    });
             }
 
             /*  expand includes  */
             if (options.expandIncludes) {
                 txt = txt.replace(options.directiveSyntax.include,
                     function (directive, prolog, _q, file, definitions, epilog) {
+                        /*  resolve possibly existing escapes  */
+                        file = file.replace(new RegExp("\\\\" + _q, "g"), _q);
 
-                    /*  resolve possibly existing escapes  */
-                    file = file.replace(new RegExp("\\\\" + _q, "g"), _q);
+                        /*  process file  */
+                        if (!grunt.file.isPathAbsolute(file))
+                            file = path.resolve(path.join(srcdir, file));
+                        if (!grunt.file.exists(file))
+                            throw "Include file \"" + file + "\" not found.";
+                        var include_dir = path.dirname(file);
+                        var include_file = path.basename(file);
+                        var txt = grunt.file.read(file);
 
-                    /*  process file  */
-                    if (!grunt.file.isPathAbsolute(file))
-                        file = path.resolve(path.join(srcdir, file));
-                    if (!grunt.file.exists(file))
-                        throw "Include file \"" + file + "\" not found.";
-                    var include_dir = path.dirname(file);
-                    var include_file = path.basename(file);
-                    var txt = grunt.file.read(file);
+                        /*  optionally strip header comment of includes  */
+                        if (options.stripHeaderOfInclude)
+                            txt = txt.replace(options.directiveSyntax.header, "");
 
-                    /*  optionally strip header comment of includes  */
-                    if (options.stripHeaderOfInclude)
-                        txt = txt.replace(options.directiveSyntax.header, "");
+                        /*  process defines  */
+                        var include_defines = {};
+                        extend(include_defines, defines);
+                        if (typeof definitions !== "undefined" && definitions !== "") {
+                            definitions.replace(options.directiveSyntax.define,
+                                function (define, _q1, name, _q2, value) {
+                                    /*  resolve possibly existing escapes  */
+                                    if (typeof _q1 !== "undefined" && _q1 !== "")
+                                        value = value.replace(new RegExp("\\\\" + _q1, "g"), _q1);
+                                    value = value.replace(new RegExp("\\\\" + _q2, "g"), _q2);
 
-                    /*  process defines  */
-                    var include_defines = defines;
-                    if (typeof definitions !== "undefined" && definitions !== "") {
-                        definitions.replace(options.directiveSyntax.define,
-                            function (define, _q1, name, _q2, value) {
-                            /*  resolve possibly existing escapes  */
-                            if (typeof _q1 !== "undefined" && _q1 !== "")
-                                value = value.replace(new RegExp("\\\\" + _q1, "g"), _q1);
-                            value = value.replace(new RegExp("\\\\" + _q2, "g"), _q2);
-
-                            /*  store definition  */
-                            include_defines[name] = value;
-                        });
-                    }
-
-                    /*  expand included text (RECURSION!)  */
-                    txt = expandText(
-                        destdir,
-                        include_dir,
-                        include_file,
-                        include_defines,
-                        txt
-                    );
-
-                    /*  post whitespace processing  */
-                    if (options.repeatWhitespaceProlog)
-                        txt = txt.replace(/^(?=.)/mg, prolog);
-                    if (options.keepWhitespaceProlog)
-                        txt = prolog + txt;
-                    if (options.keepWhitespaceEpilog)
-                        txt = txt + epilog;
-                    // substitute entities
-                    if (options.substituteEntities) {
-                        for (var entity in options.entities) {
-                            txt = txt.replace(new RegExp(entity,'g'),'&'+options.entities[entity]+';');
+                                    /*  store definition  */
+                                    include_defines[name] = value;
+                                });
                         }
-                    }
 
-                    return txt;
-                });
+                        /*  expand included text (RECURSION!)  */
+                        txt = expandText(
+                            destdir,
+                            include_dir,
+                            include_file,
+                            include_defines,
+                            txt
+                        );
+
+                        /*  post whitespace processing  */
+                        if (options.repeatWhitespaceProlog)
+                            txt = txt.replace(/^(?=.)/mg, prolog);
+                        if (options.keepWhitespaceProlog)
+                            txt = prolog + txt;
+                        if (options.keepWhitespaceEpilog)
+                            txt = txt + epilog;
+                        // substitute entities
+                        var doSubstitution = ['false','true'].indexOf(include_defines['substitution']);  // check for valid local define
+                        if (doSubstitution === -1) {
+                            doSubstitution = options.substituteEntities     // use global if no local found
+                        } else {
+                            doSubstitution = doSubstitution!=0?true:false;  // convert local to boolean
+                        }
+                        if (doSubstitution) {
+                            for (var entity in options.entities) {
+                                txt = txt.replace(new RegExp(entity,'g'),'&'+options.entities[entity]+';');
+                            }
+                        }
+
+                        return txt;
+                    });
             }
 
             /*  adjust references  */
@@ -187,27 +194,27 @@ module.exports = function (grunt) {
                 txt = txt.replace(options.directiveSyntax.adjust,
                     function (directive, prolog, _q, ref, epilog) {
 
-                    /*  resolve possibly existing escapes  */
-                    ref = ref.replace(new RegExp("\\\\" + _q, "g"), _q);
+                        /*  resolve possibly existing escapes  */
+                        ref = ref.replace(new RegExp("\\\\" + _q, "g"), _q);
 
-                    /*  only adjust references which can be resolved on the filesystem  */
-                    var file = path.join(srcdir, ref);
-                    if (grunt.file.exists(file)) {
-                        /*  adjust path from source to destination anchor  */
-                        var ref_new = path.normalize(path.join(path.relative(destdir, srcdir), ref));
+                        /*  only adjust references which can be resolved on the filesystem  */
+                        var file = path.join(srcdir, ref);
+                        if (grunt.file.exists(file)) {
+                            /*  adjust path from source to destination anchor  */
+                            var ref_new = path.normalize(path.join(path.relative(destdir, srcdir), ref));
 
-                        /*  ensure that the reference is still correct based on the destination  */
-                        file = path.join(destdir, ref_new);
-                        if (!grunt.file.exists(file))
-                            throw "Adjusted referenced file \"" + ref_new + "\" not found " +
-                                "(from destination directory \"" + destdir + "\").";
+                            /*  ensure that the reference is still correct based on the destination  */
+                            file = path.join(destdir, ref_new);
+                            if (!grunt.file.exists(file))
+                                throw "Adjusted referenced file \"" + ref_new + "\" not found " +
+                                    "(from destination directory \"" + destdir + "\").";
 
-                        /*  provide adjusted directive  */
-                        directive = prolog + ref_new + epilog;
-                    }
+                            /*  provide adjusted directive  */
+                            directive = prolog + ref_new + epilog;
+                        }
 
-                    return directive;
-                });
+                        return directive;
+                    });
             }
 
             return txt;
